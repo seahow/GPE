7a309c312a244b74d237abd1265aeec139044e18
Sean Howard <seahow@amazon.com>
2021-05-24 01:32:19 -0500

    t

--- deployment-scripts/push-multisite-gpe.sh
+++ deployment-scripts/push-multisite-gpe.sh
@@ -5,9 +5,9 @@
 #
 
 echo "creating stack in us-east-1.."
-./pushgpe.sh --r us-east-1 --c "GPE v1.1a : east" --i z1d.large --t Central --k l0-testing --b centralsa-labs --p gpe --n EAST --o suppress --x 172.17.10.0 --a east.byod --z EAST --y Aws2020@
+./pushgpe.sh --r us-east-1 --c "GPE v1.1a : east [10.1.10.0/24]" --i z1d.2xlarge --t Central --k l0-testing --b centralsa-labs --p gpe --n EAST --o suppress --x 10.1.10.0 --a byodatacenter.east --z EAST --y Aws2020@
 
 echo "creating stack in us-west-2"
-./pushgpe.sh --r us-west-2 --c "GPE v1.1a : west" --i z1d.large --t Central --k l0-testing-oregon --b centralsa-labs --p gpe --n WEST --o suppress --x 172.17.11.0 --a west.byod --z WEST --y Aws2020@
+./pushgpe.sh --r us-west-2 --c "GPE v1.1a : west [10.1.11.0/24]" --i z1d.2xlarge --t Central --k l0-testing-oregon --b centralsa-labs --p gpe --n WEST --o suppress --x 10.1.11.0 --a byodatacenter.west --z WEST --y Aws2020@
 
 echo "done"
